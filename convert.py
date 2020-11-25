7e910f36d39c15b133b008a1be808de4e8ccd4bc
David Shupe <shupe@ipac.caltech.edu>
2020-11-25 14:00:19 -0800

    update notebooks to new versions

--- convert.py
+++ convert.py
@@ -9,6 +9,8 @@ args = make_parser().parse_args()
 if args.template_file is None and os.path.exists('nb_html.tpl'):
     args.template_file = 'nb_html.tpl'
 
+print('args.template_file is {}'.format(args.template_file))
+
 if args.exclude is None and args.include is None:
     # If there is an "exclude_notebooks" file, use that to find which ones to
     # skip.  Format is one pattern per line, "#" for comments.
