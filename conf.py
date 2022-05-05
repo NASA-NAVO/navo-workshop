7d89b8c14a4513ab4367948fce9caad2fa3fc403
Brigitta Sipőcz <bsipocz@gmail.com>
2022-05-05 16:45:03 -0700

    Update renamed configitem

--- conf.py
+++ conf.py
@@ -37,8 +37,7 @@ templates_path = ['_templates']
 exclude_patterns = ['_build', 'notes']
 
 # MyST-NB configuration
-execution_timeout = 900
-
+nb_execution_timeout = 900
 
 # -- Options for HTML output -------------------------------------------------
 
